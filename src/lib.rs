3ca6449122d38f5f18ae584ce58a4501f3ba0382
Congyuwang <congyuwang@link.cuhk.edu.cn>
2022-03-13 20:04:14 +0800

    expose ConnectionInitError

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub(crate) mod network;
 mod stream_utility;
 
 pub use game::*;
-pub use lobby::{start_server, Messages, Responses, RoomState, RoomToken};
+pub use lobby::{start_server, Messages, Responses, RoomState, RoomToken, ConnectionInitError};
 pub use network::{Conn, ConnectionError, Received};
 
 pub(crate) const CHANNEL_SIZE: usize = 5;
